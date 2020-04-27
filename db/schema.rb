09fa25c30565edb7fa7cb086e4d907089a758bf2
nuri1977 <nurilacka@gmail.com>
2020-04-27 23:46:09 +0200

    Carrierwave uploader added for Shot model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200426104737) do
+ActiveRecord::Schema.define(version: 20200427213947) do
 
   create_table "shots", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20200426104737) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "user_shot"
     t.index ["user_id"], name: "index_shots_on_user_id"
   end
 
